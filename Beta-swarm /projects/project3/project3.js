cdb37c90cc4952ff4d9eb30552b2056d547a7003
TONY ANTONY <tonyantony300@gmail.com>
2023-11-10 18:33:44 +0530

    key boxes done

--- Beta-swarm /projects/project3/project3.js
+++ Beta-swarm /projects/project3/project3.js
@@ -1,46 +1,18 @@
-let circles = document.querySelectorAll(".circle");
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
+let insert = document.getElementById("insert");
 
-let currentIndex = 1;
-
-next.addEventListener("click", () => {
-  currentIndex++;
-  if (currentIndex > circles.length) {
-    currentIndex = circles.length;
-  }
-  updateDom();
+window.addEventListener("keydown", (event) => {
+  insert.innerHTML = `
+    <div class="key">
+    ${event.key === " " ? "space" : event.key}
+    <small>event.key</small>
+    </div>
+    <div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+    </div>
+    <div class="key">
+    ${event.code}
+    <small>event.code</small>
+    </div>
+      `;
 });
-
-prev.addEventListener("click", () => {
-  currentIndex--;
-  if (currentIndex < 1) {
-    currentIndex = 1;
-  }
-  updateDom();
-});
-
-function updateDom() {
-  circles.forEach((circle, index) => {
-    if (index < currentIndex) {
-      circle.classList.add("active");
-    } else {
-      circle.classList.remove("active");
-    }
-  });
-
-  let active = document.querySelectorAll(".active");
-  let progess = document.getElementById("progress");
-
-  progess.style.width =
-    ((active.length - 1) / (circles.length - 1)) * 100 + "%";
-
-  if (currentIndex === 1) {
-    prev.disabled = true;
-  } else if (currentIndex === circles.length) {
-    next.disabled = true;
-  } else {
-    prev.disabled = false;
-    next.disabled = false;
-  }
-}
