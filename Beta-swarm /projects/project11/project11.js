e00405e4ee0b303a203a2d8551e751ee42d39d1e
TONY ANTONY <tonyantony300@gmail.com>
2023-11-28 11:08:17 +0530

    responsible grid

--- Beta-swarm /projects/project11/project11.js
+++
@@ -1,8 +0,0 @@
-let container = document.querySelector(".search");
-let button = document.querySelector(".btn");
-let input = document.querySelector(".input");
-
-button.addEventListener("click", () => {
-  container.classList.toggle("active");
-  input.focus();
-});
