468230f020cecc365d82a50e0e45c713c69f7dc0
TONY ANTONY <tonyantony300@gmail.com>
2023-12-07 19:02:48 +0530

    tic tac toe code

--- project-js/index.js
+++ project-js/index.js
@@ -507,4 +507,74 @@ let minVal = Math.min(...arr);
 let occurences = arr.filter((el) => el === minVal);
 console.log(occurences.length);
 
-///
+///  react code for tic tac toe
+
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
+  function handleClick(i) {
+    if (winner || board[i]) return;
+
+    const newBoard = board.slice();
+    newBoard[i] = xIsNext ? "X" : "O";
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  }
+
+  function calculateWinner(board) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }
+
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => handleClick(i)}>
+        {board[i]}
+      </button>
+    );
+  }
+
+  function renderRestartButton() {
+    return (
+      <button onClick={() => setBoard(Array(9).fill(null))} className="restart">
+        Restart Game
+      </button>
+    );
+  }
+
+  return (
+    <div className="game">
+      <div className="board">
+        {[...Array(9).keys()].map((i) => renderSquare(i))}
+      </div>
+      <div className="info">
+        {winner ? `Winner: ${winner}` : `Next Player: ${xIsNext ? "X" : "O"}`}
+        <div>{renderRestartButton()}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/// react component ends here
