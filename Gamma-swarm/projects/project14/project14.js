b1b5ed8fa532f9e5603e62171a6ab90a478e3916
tony <tonyantony300@gmail.com>
2024-01-02 19:52:02 +0530

    Shallow comparator done

--- Gamma-swarm/projects/project14/project14.js
+++ Gamma-swarm/projects/project14/project14.js
@@ -21,3 +21,43 @@ requestManager("https://foo.com")
     console.log(response);
   })
   .catch((error) => console.log(error));
+
+//Design shallow comparison function
+
+function typeOf(input) {
+  const rawObject = Object.prototype.toString.call(input).toLowerCase();
+  console.log(rawObject);
+  const typeRegex = /\[object (.*)]/g;
+  const type = typeRegex.exec(rawObject)[1];
+  return type;
+}
+
+const shallowComparison = (source, target) => {
+  if (typeOf(source) !== typeOf(target)) {
+    return false;
+  }
+
+  if (typeOf(source) === "array") {
+    if (source.length !== target.length) {
+      return false;
+    }
+    return source.every((el, index) => el === target[index]);
+  }
+
+  if (typeOf(source) === "object") {
+    if (Object.keys(source).length !== Object.keys(target).length) {
+      return false;
+    }
+
+    return Object.keys(source).every((el) => source[el] === target[el]);
+  }
+
+  if (typeOf(source) === "date") {
+    return source.getTime() === target.getTime();
+  }
+
+  // The below is for primitives
+  return source === target;
+};
+
+console.log(shallowComparison([1], [1]));
